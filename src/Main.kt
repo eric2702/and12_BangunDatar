effd2991413a558caf2eb3ca2a4a949a6eba0c99
Sir Joseph <sirjosephholmes@gmail.com>
2022-03-09 14:58:44 +0700

    create persegi.kt

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,9 @@ fun main() {
     val persegiPanjang = PersegiPanjang(2.0, 5.2)
     persegiPanjang.hitungLuas()
     persegiPanjang.hitungKeliling()
+
+    val persegi = Persegi(1.0, 9,9)
+    persegi.hitungluas()
+    persegi.hitungKeliling()
+    
 }
